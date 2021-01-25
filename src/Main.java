762c89ee6a8bbd258a3e7a65ecf3f100aa8a8ade
Авет <avet22@list.ru>
2021-01-26 00:48:41 +0300

    eleven

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] agrs) {
         int price = 3000_11;
         int mile = 20;
-        int bonus = (price/100) / mile;
+        int bonus = (price / 100) / mile;
         System.out.println(bonus);
     }
 }
